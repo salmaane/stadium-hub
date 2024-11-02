15f04070008f7129809d12c3c7e53370126e2de2
salmane <elmourtajisalmane@gmail.com>
2024-11-03 00:08:41 +0100

    created gateway service

---
+++ gateway-service/src/test/java/org/salmane/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.salmane.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
